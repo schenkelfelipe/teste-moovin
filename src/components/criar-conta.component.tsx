56ebf42bfdf1f8c4993991dbf0c1b81750eec71f
FSchenkel <felipe_s1@hotmail.com>
2020-02-10 10:16:25 -0300

    ajuste component react

--- src/components/criar-conta.component.tsx
+++ src/components/criar-conta.component.tsx
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React from "react";
 import Form from 'react-bootstrap/Form';
 import axios from 'axios';
 import Button from 'react-bootstrap/Button';
